598a987237ae19b1af555a471cf015022e1bc75a
Filaton <49982385+Filaton@users.noreply.github.com>
2021-06-20 19:14:02 +0200

    Added web app

---
+++ WebApp/DapiWebApp/Models/ViewCocktail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapiWebApp.Models
+{
+    public class ViewCocktail
+    {
+        public string Cocktails { get; set; }
+        public string Zutat { get; set; }
+        public int Menge { get; set; }
+    }
+}
