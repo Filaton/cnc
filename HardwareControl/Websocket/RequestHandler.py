4bc3b526a7cf79229fa8c6ac9ae5f574ecafd521
Hendrik Marske <Hendrik@familie-marske.de>
2021-08-02 17:13:12 +0200

    first testing

--- HardwareControl/Websocket/RequestHandler.py
+++ HardwareControl/Websocket/RequestHandler.py
@@ -1,14 +1,14 @@
-from http.server import HTTPServer
-from http.server import SimpleHTTPRequestHandler
+#!/usr/bin/python3
+import http.server
 import socketserver
 import logging
 from multiprocessing import Process
 
-logging.basicConfig(filename="./log.log", format='%(asctime)s %(message)s', level=logging.INFO)
+#logging.basicConfig(filename="./log.log", format='%(asctime)s %(message)s', level=logging.INFO)
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 socketserver.TCPServer.allow_reuse_address = True
 
-def standard_func():
+def standard_func(input):
     return "0"
 
 CommandList = {
@@ -17,21 +17,28 @@ CommandList = {
 
 
 
-class ServerHandler(SimpleHTTPRequestHandler):
+class ServerHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        SimpleHTTPRequestHandler.do_GET(self)
+        http.server.SimpleHTTPRequestHandler.do_GET(self)
 
     def do_POST(self):
+        logging.debug(self.version_string())
         logging.debug("PostRequestHandler - Start")
         global CommandList
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        post_data = post_data.decode("utf-8")
-        post_data = post_data.split(';')
-        func = CommandList.get(post_data[0])
-        ret_data = func(post_data[1])
-        self.wfile.write(ret_data.encode("utf-8"))
+        self.data = self.rfile.read(content_length).strip() # <--- Gets the data itself
+        logging.debug(self.data)
+        #func = CommandList.get(post_data[0])
+        #ret_data = func(post_data[1])
+        self.wfile.write(self._html(self.data))
         logging.debug("Post Request Handled - RETURNING")
+
+    def _html(self, message):
+        """This just generates an HTML document that includes `message`
+        in the body. Override, or re-write this do do more interesting stuff.
+        """
+        content = f"<html><body><h1>{message}</h1></body></html>"
+        return content.encode("utf8")  # NOTE: must return a bytes object!
 
 class RequestHandler(object):
     """
@@ -54,7 +61,8 @@ class RequestHandler(object):
     def join(self):
         self.process.join()
 
-def test():
+def test(input):
+    logging.debug("in TEST")
     return "DONE"
 
 tempCommand = {
@@ -62,8 +70,16 @@ tempCommand = {
 }
 
 if __name__ == "__main__":
-    r = RequestHandler(tempCommand,True)
-    r.join()
+    Server = socketserver.TCPServer(("",8000),ServerHandler)
+    Server.serve_forever()
+
+    
+    
+    
+    
+    #r = RequestHandler(tempCommand,True)
+    #
+    #r.join()
 
 
 
